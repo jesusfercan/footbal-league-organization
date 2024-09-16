dbc535d6cc077154a850b1d7b16dc26645f20e85
Jesus <jesusfercan@gmail.com>
2024-09-16 09:42:43 +0200

    first commit

---
+++ src/main/java/es/jfcode/football_league_organization/FootballLeagueOrganizationApplication.java
@@ -0,0 +1,13 @@
+package es.jfcode.football_league_organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLeagueOrganizationApplication.class, args);
+	}
+
+}
